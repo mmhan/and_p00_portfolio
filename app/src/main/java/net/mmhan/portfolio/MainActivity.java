5a72f505602ad30f9e345ff81bb821ea32c4c93c
mmhan <mmhan2u@gmail.com>
2015-07-20 02:00:52 +0630

    Completed the instructions

--- app/src/main/java/net/mmhan/portfolio/MainActivity.java
+++ app/src/main/java/net/mmhan/portfolio/MainActivity.java
@@ -4,14 +4,37 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+
+    Button mSpotifyBtn, mScoresAppBtn, mLibraryAppBtn, mBuildItBiggerBtn, mXyzReaderBtn, mCapstoneBtn;
+
+    ArrayList<Button> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSpotifyBtn = (Button) findViewById(R.id.spotify_streamer_btn);
+        mSpotifyBtn.setOnClickListener(this);
+        mScoresAppBtn = (Button) findViewById(R.id.scores_app_btn);
+        mScoresAppBtn.setOnClickListener(this);
+        mLibraryAppBtn = (Button) findViewById(R.id.library_app_btn);
+        mLibraryAppBtn.setOnClickListener(this);
+        mBuildItBiggerBtn = (Button) findViewById(R.id.build_it_bigger_btn);
+        mBuildItBiggerBtn.setOnClickListener(this);
+        mXyzReaderBtn = (Button) findViewById(R.id.xyz_reader_btn);
+        mXyzReaderBtn.setOnClickListener(this);
+        mCapstoneBtn = (Button) findViewById(R.id.my_own_app_btn);
+        mCapstoneBtn.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +57,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button btn = (Button) view;
+        Toast.makeText(this,
+                String.format("This button will launch %s", btn.getText()),
+                Toast.LENGTH_LONG).show();
     }
 }
